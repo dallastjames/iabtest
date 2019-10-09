f5066c56fbcb52d481f30e8695307f0bee560f7a
Dallas James <dallastjames@gmail.com>
2019-10-09 13:33:44 -0600

    init to win it

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss']
 })
 export class HomePage {
+    constructor(private iab: InAppBrowser) {}
 
-  constructor() {}
-
+    public openIAB(): void {
+        this.iab.create('/assets/ng/callback.html');
+    }
 }
